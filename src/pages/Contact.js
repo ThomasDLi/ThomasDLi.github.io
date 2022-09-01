424d85785a0b605e1317dc069052614cd01d3394
ThomasDLi <lithomas753@gmail.com>
2022-08-31 19:47:20 -0700

    wrote some text, added some info, fixed some cluttering issues

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <h1>Contact Me!</h1>
       <h2>{">"} <a target="_blank" href="https://github.com/ThomasDLi" rel="noopener noreferrer">Github</a></h2>
       <h2>{">"} <a target="_blank" href="https://dmoj.ca/user/JamesBrook123" rel="noopener noreferrer">DMOJ</a></h2>
-      <h2>{">"} thomas.li26@stgeorges.bc.ca</h2>
+      <h2>{">"} <a target="_blank" href="mailto: thomas.li26@stgeorges.bc.ca" rel="noopener noreferrer">Email</a></h2>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,9 @@ function Home() {
   return (
     <div style={css} className="home">
       <h1>👋 Hey, I'm Thomas!</h1>
-      <h2>I'm a software dev and a hardware tinkerer.</h2>
-      <h2>I have skills in electronics, CS, DS, CAD, and more!</h2>
-      <div style={css} className="links">
-        <Link to="/projects"><h2>My Projects</h2></Link>
-        <Link to="/contact"><h2>Contact me</h2></Link>
-      </div>
+      <h2>I program computers, design circuts, create products, and I'm interested in everything tech.</h2>
+      <h2>When I'm not staring at my screen, I love to bike, paint, and make music.</h2>
+      <h2>Please Consider checking out <Link to="/projects">my projects</Link> or <Link to="/contact">contacting me</Link>!</h2>
     </div>
   );
 }
